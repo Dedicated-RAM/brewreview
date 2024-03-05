161f61f6cc3aa16a7b4ff4ea6cdffea873d8ae18
Aidan Haberman <aidanhaberman@icloud.com>
2024-03-05 15:58:41 -0500

    firebase implementation (WIP)

---
+++ src/firebase/FirebaseConfig.js
@@ -0,0 +1,15 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+  apiKey: FIREBASE_KEY,
+  authDomain: FIREBASE_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
+};
+
