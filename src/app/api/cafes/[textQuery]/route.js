fff9ecd51494bffb41818de50d879fa74d60918d
brrryry <thisisbryanchan@gmail.com>
2024-04-29 17:09:28 -0400

    Added basic google maps API functionality (search by text) on /api/cafes/:textQuery, installed .env library

---
+++ src/app/api/cafes/[textQuery]/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+import * as helpers from "../../../../data/validators";
+
+export async function GET(req, { params }) {
+    try {
+        params.textQuery = helpers.isValidString(
+            params.textQuery,
+            "Text Query"
+        );
+
+        const res = await axios.get(
+            `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(
+                params.textQuery
+            )}&inputtype=textquery&key=${encodeURIComponent(
+                process.env.GOOGLE_MAPS_API_KEY
+            )}`
+        );
+
+        console.log("Google Places API Response:", res.data);
+
+        return NextResponse.json({ result: res.data });
+    } catch (e) {
+        console.error("Error:", e.response ? e.response.data : e.message);
+        return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+    }
+}
