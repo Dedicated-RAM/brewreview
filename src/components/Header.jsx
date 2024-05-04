e2b8ac83ab1cf3ee8cc941f71a26e2285685bfc0
Yihan Jiang <yihanjiang04@gmail.com>
2024-05-04 18:41:57 -0400

    Made a basic navigation bar but needs to have some backend to do the login

---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import "../styles/globals.css";
+
+export default function Header() {
+  function useUserSession(initialUser) {
+    const [user, setUser] = useState(initialUser);
+    const router = useRouter();
+
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged((authUser) => {
+        setUser(authUser);
+      });
+      return () => {
+        unsubscribe();
+      };
+    }, []);
+
+    useEffect(() => {
+      onAuthStateChanged((authUser) => {
+        if (user === undefined) return;
+        if (user?.email !== authUser?.email) {
+          router.refresh();
+        }
+      });
+    }, [user]);
+
+    return user;
+  }
+
+  return (
+    <div class="navbar bg-accent-3 font-short-stack">
+      <Link href="/" class="btn btn-ghost text-4xl bg-accent-6 text-accent-2">
+        Brew Review
+      </Link>
+      <Link href="/group/list" class="btn btn-ghost text-xl">
+        View Groups
+      </Link>
+      <Link href="/group/create" class="btn btn-ghost text-xl">
+        Create Groups
+      </Link>
+      <Link href="/login" className="btn btn-ghost text-xl ml-auto">
+        Login
+      </Link>
+    </div>
+  );
+}
---
+++ src/pages/_app.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
+}
