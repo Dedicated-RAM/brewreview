2efee0a979c9efea7c7fd46f7a299adb404c5478
Britney Yang <byang.careers@gmail.com>
2024-05-05 00:04:56 -0400

    update side panel

--- src/components/SidePanelGoogleReview.jsx
+++ src/components/SidePanelGoogleReview.jsx
@@ -8,26 +8,26 @@ export default function SidePanelGoogleReview({ place }) {
   return (
     <div className="">
       {
-        place.reviews.map((review, index) => {
+        place?.reviews.map((review, index) => {
           return (
             <div className="m-3 p-3 bg-accent-2 rounded-xl" key={index}>
               <div className="flex items-center">
                 <img
                   alt="Reviewer Profile Picture"
                   className="w-10 h-10 overflow-hidden rounded-full"
-                  src={review.profile_photo_url}
+                  src={review?.profile_photo_url}
                 />
-                <span className="pl-2 font-semibold text-base">{review.author_name}</span>
+                <span className="pl-2 font-semibold text-base">{review?.author_name}</span>
               </div>
               <div className="flex space-x-2 items-center">
-                <span className="text-accent-4 text-sm">{review.rating} stars</span>
+                <span className="text-accent-4 text-sm">{review?.rating} stars</span>
                 <span className="text-accent-4 text-lg">
-                  {'★'.repeat(Math.floor(review.rating))}
+                  {'★'.repeat(Math.floor(review?.rating))}
                 </span>
-                <span className="text-sm text-accent-4">{review.relative_time_description}</span>
+                <span className="text-sm text-accent-4">{review?.relative_time_description}</span>
               </div>
               <p className="text-sm">
-                {review.text}
+                {review?.text}
               </p>
             </div>
           );
