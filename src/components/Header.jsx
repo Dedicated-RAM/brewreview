1e7e00bac644119866997f98ce79a72ba191c43a
brrryry <thisisbryanchan@gmail.com>
2024-05-06 04:43:48 -0400

    Adding more auth functionality with the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,46 +7,40 @@ import Link from "next/link";
 import "../styles/globals.css";
 
 export default function Header() {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (authUser) => {
-            setUser(authUser ? authUser : null);
-        });
-    }, [user, auth]);
+  useEffect(() => {
+    onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser ? authUser : null);
+    });
+  }, [user, auth]);
 
-    return (
-        <header>
-            <div className="navbar bg-accent-3 font-short-stack">
-                <Link
-                    href="/"
-                    className="btn btn-ghost text-4xl bg-accent-6 text-accent-2"
-                >
-                    Brew Review
-                </Link>
-                <Link href="/group/list" className="btn btn-ghost text-xl">
-                    View Groups
-                </Link>
-                <Link href="/group/create" className="btn btn-ghost text-xl">
-                    Create Groups
-                </Link>
-                {!user && (
-                    <Link
-                        href="/login"
-                        className="btn btn-ghost text-xl ml-auto"
-                    >
-                        Login
-                    </Link>
-                )}
-                {user && (
-                    <Link
-                        href="/logout"
-                        className="btn btn-ghost text-xl ml-auto"
-                    >
-                        Logout
-                    </Link>
-                )}
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="navbar bg-accent-3 font-short-stack">
+        <Link
+          href="/"
+          className="btn btn-ghost text-4xl bg-accent-6 text-accent-2"
+        >
+          Brew Review
+        </Link>
+        <Link href="/group/list" className="btn btn-ghost text-xl">
+          View Groups
+        </Link>
+        <Link href="/group/create" className="btn btn-ghost text-xl">
+          Create Groups
+        </Link>
+        {!user && (
+          <Link href="/login" className="btn btn-ghost text-xl ml-auto">
+            Login
+          </Link>
+        )}
+        {user && (
+          <Link href="/logout" className="btn btn-ghost text-xl ml-auto">
+            Logout
+          </Link>
+        )}
+      </div>
+    </header>
+  );
 }
--- src/pages/logout/index.jsx
+++ src/pages/logout/index.jsx
@@ -4,30 +4,30 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import "../../styles/globals.css";
 import {
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signOut,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { auth } from "../../lib/firebase/FirebaseConfig";
 import { useRouter } from "next/router";
 //import { doSignInWithEmailAndPassword } from "../../lib/firebase/auth.js";
 
 export default function Logout() {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (authUser) => {
-            if (authUser) setUser(authUser);
-        });
-    }, []);
+  useEffect(() => {
+    onAuthStateChanged(auth, (authUser) => {
+      if (authUser) setUser(authUser);
+    });
+  }, []);
 
-    const router = useRouter();
+  const router = useRouter();
 
-    if (!user) {
-        return <p>You weren't logged in!</p>;
-    } else {
-        signOut(auth).then(() => {
-            router.push("/");
-        });
-    }
+  if (!user) {
+    return <p>You were not logged in!</p>;
+  } else {
+    signOut(auth).then(() => {
+      router.push("/");
+    });
+  }
 }
