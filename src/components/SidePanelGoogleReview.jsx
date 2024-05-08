a1e828b69fc59d2e82c55127446f78cd5f444d95
brrryry <thisisbryanchan@gmail.com>
2024-05-08 13:47:08 -0400

    Removed debug statement on google reviews

--- src/components/SidePanelGoogleReview.jsx
+++ src/components/SidePanelGoogleReview.jsx
@@ -3,45 +3,44 @@ import { useEffect } from "react";
 import Image from "next/image";
 
 export default function SidePanelGoogleReview({ place }) {
-  useEffect(() => {}, [place]);
+    useEffect(() => {}, [place]);
 
-  if (!place || Object.keys(place).length <= 0)
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    );
-  return (
-    <div className="">
-      {place?.reviews.map((review, index) => {
-        console.log(review);
+    if (!place || Object.keys(place).length <= 0)
         return (
-          <div className="m-3 p-3 bg-accent-2 rounded-xl" key={index}>
-            <div className="flex items-center">
-              <img
-                alt="Reviewer Profile Picture"
-                className="w-8 h-8 overflow-hidden rounded-full"
-                src={review?.profile_photo_url}
-              />
-              <span className="pl-2 font-semibold text-base">
-                {review?.author_name}
-              </span>
+            <div>
+                <p>Loading...</p>
             </div>
-            <div className="flex space-x-2 items-center">
-              <span className="text-accent-4 text-sm">
-                {review?.rating} stars
-              </span>
-              <span className="text-accent-4 text-lg">
-                {"★".repeat(Math.floor(review?.rating))}
-              </span>
-              <span className="text-sm text-accent-4">
-                {review?.relative_time_description}
-              </span>
-            </div>
-            <p className="text-sm">{review?.text}</p>
-          </div>
         );
-      })}
-    </div>
-  );
+    return (
+        <div className="">
+            {place?.reviews.map((review, index) => {
+                return (
+                    <div className="m-3 p-3 bg-accent-2 rounded-xl" key={index}>
+                        <div className="flex items-center">
+                            <img
+                                alt="Reviewer Profile Picture"
+                                className="w-8 h-8 overflow-hidden rounded-full"
+                                src={review?.profile_photo_url}
+                            />
+                            <span className="pl-2 font-semibold text-base">
+                                {review?.author_name}
+                            </span>
+                        </div>
+                        <div className="flex space-x-2 items-center">
+                            <span className="text-accent-4 text-sm">
+                                {review?.rating} stars
+                            </span>
+                            <span className="text-accent-4 text-lg">
+                                {"★".repeat(Math.floor(review?.rating))}
+                            </span>
+                            <span className="text-sm text-accent-4">
+                                {review?.relative_time_description}
+                            </span>
+                        </div>
+                        <p className="text-sm">{review?.text}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
