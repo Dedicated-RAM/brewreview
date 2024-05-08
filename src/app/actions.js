a06f24c1fbbaa0f09218500d900f0638b8adaad3
brrryry <thisisbryanchan@gmail.com>
2024-05-08 17:02:34 -0400

    Clickable Map

--- src/app/actions.js
+++ src/app/actions.js
@@ -5,19 +5,19 @@ import { getAuthenticatedAppForUser } from "@/src/lib/firebase/firebase";
 import { getFirestore } from "firebase/firestore";
 
 export async function handleReviewFormSubmission(data) {
-  const { app } = await getAuthenticatedAppForUser();
-  const db = getFirestore(app);
+    const { app } = await getAuthenticatedAppForUser();
+    const db = getFirestore(app);
 
-  //TODO: validate data?
+    //TODO: validate data?
 
-  await addReview(db, data.get("place_id"), {
-    overall_rating: data.get("overall_rating"),
-    number_of_seats: data.get("number_of_seats"),
-    outlet_rating: data.get("outlet_rating"),
-    word_review: data.get("word_review"),
-    wifi: data.get("wifi"),
-    user_id: data.get("user_id"),
-  });
+    await addReview(db, data.get("place_id"), {
+        overall_rating: data.get("overall_rating"),
+        number_of_seats: data.get("number_of_seats"),
+        outlet_rating: data.get("outlet_rating"),
+        word_review: data.get("word_review"),
+        wifi: data.get("wifi"),
+        user_id: data.get("user_id"),
+    });
 }
 
 // This is a next.js server action, an alpha feature, so
