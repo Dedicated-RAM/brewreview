82503576c5f2596832a0bbbf541d3464f31984b5
Britney Yang <byang.careers@gmail.com>
2024-05-08 20:07:27 -0400

    middleware

--- src/pages/logout/index.jsx
+++ src/pages/logout/index.jsx
@@ -5,12 +5,24 @@ import Link from "next/link";
 import "../../styles/globals.css";
 import { doSignOut } from "../../lib/firebase/firebase";
 import { useRouter } from "next/router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../../lib/firebase/FirebaseConfig";
+
 
 export default function Logout() {
   const router = useRouter();
 
-  (async () => {
-    await doSignOut();
-    router.push("/");
-  })();
+  useEffect(() => {
+    onAuthStateChanged(auth, (authUser) => {
+      if (!authUser) router.push("/login");
+      else {
+        (async () => {
+          await doSignOut();
+          router.push("/");
+        })();
+      }
+    });
+  }, []);
+
+
 }
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import "../../styles/globals.css";
 import { doCreateUserWithEmailAndPassword } from "../../lib/firebase/firebase";
+import { auth } from "../../lib/firebase/FirebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function Register() {
   const [email, setEmail] = useState("");
@@ -14,6 +16,12 @@ export default function Register() {
   const [errors, setErrors] = useState([]);
 
   const router = useRouter();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (authUser) => {
+      if (authUser) router.push("/");
+    });
+  }, []);
 
   const validateForm = () => {
     let errorList = [];
