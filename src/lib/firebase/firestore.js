943b29eb6c5be107090ac8058bff3e2e4abd238d
brrryry <thisisbryanchan@gmail.com>
2024-05-08 22:09:54 -0400

    Fixed bug where you could see events in the past

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -1,22 +1,33 @@
 import {
-  collection,
-  onSnapshot,
-  query,
-  getDocs,
-  doc,
-  getDoc,
-  updateDoc,
-  orderBy,
-  Timestamp,
-  runTransaction,
-  where,
-  addDoc,
-  setDoc,
-  getFirestore,
+    collection,
+    onSnapshot,
+    query,
+    getDocs,
+    doc,
+    getDoc,
+    updateDoc,
+    orderBy,
+    Timestamp,
+    runTransaction,
+    where,
+    addDoc,
+    setDoc,
+    getFirestore,
 } from "firebase/firestore";
 
 import { db } from "./FirebaseConfig";
 import { add } from "lodash";
+import moment from "moment";
+
+const rateLimitStore = {};
+
+const MAX_REQUESTS = 50;
+const RATE_LIMIT_TIME = 1000 * 60 * 15; // 15 minutes
+const DEBUG = true;
+
+function printRateLimitStore() {
+    console.log(rateLimitStore);
+}
 
 /**
  * Adds a review to a cafe
@@ -25,22 +36,38 @@ import { add } from "lodash";
  * @param {Object} review	The review object to add to the cafe
  */
 export async function addReview(place_id, review) {
-  // check if cafe exists in db
-  var cafe = await getCafeByPlaceId(place_id);
-  // if cafe does not exist, add it to db
-  if (!cafe) cafe = await addCafe(db, place_id);
+    // Check rate limit
+    const currentTime = Date.now();
+    const userRequests = rateLimitStore["addReview"] || [];
+    const requestsInWindow = userRequests.filter(
+        (time) => currentTime - time < RATE_LIMIT_TIME
+    );
+    rateLimitStore["addReview"] = requestsInWindow;
 
-  const cafeId = cafe.id;
-  const cafeRef = doc(db, "cafes", cafeId);
-  const reviewsRef = collection(cafeRef, "reviews");
+    if (requestsInWindow.length >= MAX_REQUESTS) {
+        throw new Error("addReview: Rate limit exceeded");
+    }
 
-  const reviewData = {
-    ...review,
-    created_at: Timestamp.now(),
-    updated_at: Timestamp.now(),
-  };
+    // Add current request to store
+    rateLimitStore["addReview"].push(currentTime);
 
-  await addDoc(reviewsRef, reviewData);
+    // check if cafe exists in db
+    var cafe = await getCafeByPlaceId(place_id);
+    // if cafe does not exist, add it to db
+    if (!cafe) cafe = await addCafe(db, place_id);
+    if (DEBUG) printRateLimitStore();
+
+    const cafeId = cafe.id;
+    const cafeRef = doc(db, "cafes", cafeId);
+    const reviewsRef = collection(cafeRef, "reviews");
+
+    const reviewData = {
+        ...review,
+        created_at: Timestamp.now(),
+        updated_at: Timestamp.now(),
+    };
+
+    await addDoc(reviewsRef, reviewData);
 }
 
 /**
@@ -51,15 +78,31 @@ export async function addReview(place_id, review) {
  * @param {Object} review	The review object to update the review with
  */
 export async function editReview(db, cafeId, reviewId, review) {
-  const cafeRef = doc(db, "cafes", cafeId);
-  const reviewRef = doc(collection(cafeRef, "reviews"), reviewId);
+    // Check rate limit
+    const currentTime = Date.now();
+    const userRequests = rateLimitStore["editReview"] || [];
+    const requestsInWindow = userRequests.filter(
+        (time) => currentTime - time < RATE_LIMIT_TIME
+    );
+    rateLimitStore["editReview"] = requestsInWindow;
 
-  const reviewData = {
-    ...review,
-    updated_at: Timestamp.now(),
-  };
+    if (requestsInWindow.length >= MAX_REQUESTS) {
+        throw new Error("editReview: Rate limit exceeded");
+    }
 
-  await updateDoc(reviewRef, reviewData);
+    // Add current request to store
+    rateLimitStore["editReview"].push(currentTime);
+    if (DEBUG) printRateLimitStore();
+
+    const cafeRef = doc(db, "cafes", cafeId);
+    const reviewRef = doc(collection(cafeRef, "reviews"), reviewId);
+
+    const reviewData = {
+        ...review,
+        updated_at: Timestamp.now(),
+    };
+
+    await updateDoc(reviewRef, reviewData);
 }
 
 /**
@@ -68,18 +111,34 @@ export async function editReview(db, cafeId, reviewId, review) {
  * @returns {Array}	An array of reviews for the cafe ordered by newest first
  */
 export async function getCafeReviews(cafeId) {
-  const cafeRef = doc(db, "cafes", cafeId);
-  const reviewsRef = collection(cafeRef, "reviews");
+    // Check rate limit
+    const currentTime = Date.now();
+    const userRequests = rateLimitStore["getCafeReviews"] || [];
+    const requestsInWindow = userRequests.filter(
+        (time) => currentTime - time < RATE_LIMIT_TIME
+    );
+    rateLimitStore["getCafeReviews"] = requestsInWindow;
 
-  const q = query(reviewsRef, orderBy("updated_at", "desc"));
-  const results = await getDocs(q);
+    if (requestsInWindow.length >= MAX_REQUESTS) {
+        throw new Error("getCafeReviews: Rate limit exceeded");
+    }
 
-  return results.docs.map((doc) => {
-    return {
-      id: doc.id,
-      ...doc.data(),
-    };
-  });
+    // Add current request to store
+    rateLimitStore["getCafeReviews"].push(currentTime);
+    if (DEBUG) printRateLimitStore();
+
+    const cafeRef = doc(db, "cafes", cafeId);
+    const reviewsRef = collection(cafeRef, "reviews");
+
+    const q = query(reviewsRef, orderBy("updated_at", "desc"));
+    const results = await getDocs(q);
+
+    return results.docs.map((doc) => {
+        return {
+            id: doc.id,
+            ...doc.data(),
+        };
+    });
 }
 
 /**
@@ -89,17 +148,33 @@ export async function getCafeReviews(cafeId) {
  * @returns {Object}	The cafe object
  */
 async function addCafe(db, place_id) {
-  const cafeRef = collection(db, "cafes");
-  const cafeData = {
-    maps_place_id: place_id,
-  };
-  // add the cafe to the database
-  const docRef = await addDoc(cafeRef, cafeData);
-  // return the cafe object
-  return {
-    id: docRef.id,
-    ...cafeData,
-  };
+    // Check rate limit
+    const currentTime = Date.now();
+    const userRequests = rateLimitStore["addCafe"] || [];
+    const requestsInWindow = userRequests.filter(
+        (time) => currentTime - time < RATE_LIMIT_TIME
+    );
+    rateLimitStore["addCafe"] = requestsInWindow;
+
+    if (requestsInWindow.length >= MAX_REQUESTS) {
+        throw new Error("addCafe: Rate limit exceeded");
+    }
+
+    // Add current request to store
+    rateLimitStore["addCafe"].push(currentTime);
+    if (DEBUG) printRateLimitStore();
+
+    const cafeRef = collection(db, "cafes");
+    const cafeData = {
+        maps_place_id: place_id,
+    };
+    // add the cafe to the database
+    const docRef = await addDoc(cafeRef, cafeData);
+    // return the cafe object
+    return {
+        id: docRef.id,
+        ...cafeData,
+    };
 }
 
 /**
@@ -109,21 +184,63 @@ async function addCafe(db, place_id) {
  * @returns {null}	If the cafe does not exist
  */
 export async function getCafeByPlaceId(place_id) {
-  const q = query(
-    collection(db, "cafes"),
-    where("maps_place_id", "==", place_id),
-  );
-  const results = await getDocs(q);
+    // Check rate limit
+    const currentTime = Date.now();
+    const userRequests = rateLimitStore["getCafeByPlaceId"] || [];
+    const requestsInWindow = userRequests.filter(
+        (time) => currentTime - time < RATE_LIMIT_TIME
+    );
+    rateLimitStore["getCafeByPlaceId"] = requestsInWindow;
 
-  if (results.empty) {
-    return null;
-  }
+    if (requestsInWindow.length >= MAX_REQUESTS) {
+        throw new Error("getCafeByPlaceId: Rate limit exceeded");
+    }
 
-  const doc = results.docs[0];
-  return {
-    id: doc.id,
-    ...doc.data(),
-  };
+    // Add current request to store
+    rateLimitStore["getCafeByPlaceId"].push(currentTime);
+    if (DEBUG) printRateLimitStore();
+
+    const q = query(
+        collection(db, "cafes"),
+        where("maps_place_id", "==", place_id)
+    );
+    const results = await getDocs(q);
+
+    if (results.empty) {
+        return null;
+    }
+
+    const doc = results.docs[0];
+    return {
+        id: doc.id,
+        ...doc.data(),
+    };
+}
+
+export async function getCafe(cafeId) {
+    // Check rate limit
+
+    const currentTime = Date.now();
+    const userRequests = rateLimitStore["getCafe"] || [];
+    const requestsInWindow = userRequests.filter(
+        (time) => currentTime - time < RATE_LIMIT_TIME
+    );
+    rateLimitStore["getCafe"] = requestsInWindow;
+
+    if (requestsInWindow.length >= MAX_REQUESTS) {
+        throw new Error("getCafe: Rate limit exceeded");
+    }
+
+    // Add current request to store
+    rateLimitStore["getCafe"].push(currentTime);
+    if (DEBUG) printRateLimitStore();
+
+    const docRef = doc(db, "cafes", cafeId);
+    const docSnap = await getDoc(docRef);
+    return {
+        id: docSnap.id,
+        ...docSnap.data(),
+    };
 }
 
 /**
@@ -132,12 +249,28 @@ export async function getCafeByPlaceId(place_id) {
  * @returns {Object}	The group object
  */
 export async function getGroupById(groupId) {
-  const docRef = doc(db, "groups", groupId);
-  const docSnap = await getDoc(docRef);
-  return {
-    id: docSnap.id,
-    ...docSnap.data(),
-  };
+    // Check rate limit
+    const currentTime = Date.now();
+    const userRequests = rateLimitStore["getGroupById"] || [];
+    const requestsInWindow = userRequests.filter(
+        (time) => currentTime - time < RATE_LIMIT_TIME
+    );
+    rateLimitStore["getGroupById"] = requestsInWindow;
+
+    if (requestsInWindow.length >= MAX_REQUESTS) {
+        throw new Error("getGroupById: Rate limit exceeded");
+    }
+
+    // Add current request to store
+    rateLimitStore["getGroupById"].push(currentTime);
+    if (DEBUG) printRateLimitStore();
+
+    const docRef = doc(db, "groups", groupId);
+    const docSnap = await getDoc(docRef);
+    return {
+        id: docSnap.id,
+        ...docSnap.data(),
+    };
 }
 
 /**
@@ -145,15 +278,37 @@ export async function getGroupById(groupId) {
  * @returns {Array}	An array of all groups
  */
 export async function getGroups() {
-  const q = query(collection(db, "groups"));
-  const results = await getDocs(q);
+    // Check rate limit
+    const currentTime = Date.now();
+    const userRequests = rateLimitStore["getGroups"] || [];
+    const requestsInWindow = userRequests.filter(
+        (time) => currentTime - time < RATE_LIMIT_TIME
+    );
+    rateLimitStore["getGroups"] = requestsInWindow;
 
-  return results.docs.map((doc) => {
-    return {
-      id: doc.id,
-      ...doc.data(),
-    };
-  });
+    if (requestsInWindow.length >= MAX_REQUESTS) {
+        throw new Error("getGroups: Rate limit exceeded");
+    }
+
+    // Add current request to store
+    rateLimitStore["getGroups"].push(currentTime);
+    if (DEBUG) printRateLimitStore();
+
+    const q = query(collection(db, "groups"));
+    const results = await getDocs(q);
+
+    return results.docs
+        .filter((doc) => {
+            return moment(`${doc.data().date} ${doc.data().time}`).isAfter(
+                moment()
+            );
+        })
+        .map((doc) => {
+            return {
+                id: doc.id,
+                ...doc.data(),
+            };
+        });
 }
 
 /**
@@ -164,10 +319,27 @@ export async function getGroups() {
  */
 
 export async function addGroup(group) {
-  await addDoc(collection(db, "groups"), group);
+    // Check rate limit
+    const currentTime = Date.now();
+    const userRequests = rateLimitStore["addGroup"] || [];
+    const requestsInWindow = userRequests.filter(
+        (time) => currentTime - time < RATE_LIMIT_TIME
+    );
+    rateLimitStore["addGroup"] = requestsInWindow;
+
+    if (requestsInWindow.length >= MAX_REQUESTS) {
+        throw new Error("addGroup: Rate limit exceeded");
+    }
+
+    // Add current request to store
+    rateLimitStore["addGroup"].push(currentTime);
+    if (DEBUG) printRateLimitStore();
+
+    await addDoc(collection(db, "groups"), group);
 }
 
 export async function editGroup(groupId, group) {
-  const groupRef = doc(db, "groups", groupId);
-  await updateDoc(groupRef, group);
+    const groupRef = doc(db, "groups", groupId);
+
+    await updateDoc(groupRef, group);
 }
--- src/pages/group/list/index.jsx
+++ src/pages/group/list/index.jsx
@@ -9,97 +9,109 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../../lib/firebase/FirebaseConfig";
 
 import {
-  getGroups,
-  getGroupsById,
-  editGroup,
+    getGroups,
+    getGroupsById,
+    editGroup,
 } from "../../../lib/firebase/firestore";
 
 export default function Group() {
-  const [groups, setGroups] = useState([]);
-  const [user, setUser] = useState(null);
+    const [groups, setGroups] = useState([]);
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const groups = await getGroups();
-      setGroups(groups);
-    })();
-  });
+    useEffect(() => {
+        (async () => {
+            const groups = await getGroups();
+            setGroups(groups);
+        })();
+    }, []);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (authUser) => {
-      setUser(authUser ? authUser : null);
-    });
-  }, [user, auth]);
+    useEffect(() => {
+        onAuthStateChanged(auth, (authUser) => {
+            setUser(authUser ? authUser : null);
+        });
+    }, [user, auth]);
 
-  const joinGroup = (groupId) => {
-    if (!auth.currentUser) Router.push("/login");
-    const group = groups.find((group) => group.id === groupId);
-    if (group.members.length < group.max_count) {
-      editGroup(groupId, {
-        members: [...group.members, auth.currentUser.displayName],
-      });
-    }
-  };
+    const joinGroup = (groupId) => {
+        if (!auth.currentUser) Router.push("/login");
+        const group = groups.find((group) => group.id === groupId);
+        if (group.members.length < group.max_count) {
+            editGroup(groupId, {
+                members: [...group.members, auth.currentUser.displayName],
+            });
+        } else alert("ERROR: Cannot join group due to max limit.");
+    };
 
-  const leaveGroup = (groupId) => {
-    if (!auth.currentUser) Router.push("/login");
-    const group = groups.find((group) => group.id === groupId);
-    editGroup(groupId, {
-      members: group.members.filter(
-        (member) => member !== auth.currentUser.displayName,
-      ),
-    });
-  };
+    const leaveGroup = (groupId) => {
+        if (!auth.currentUser) Router.push("/login");
+        const group = groups.find((group) => group.id === groupId);
+        editGroup(groupId, {
+            members: group.members.filter(
+                (member) => member !== auth.currentUser.displayName
+            ),
+        });
+    };
 
-  return (
-    <div className="bg-accent-1 inline-flex font-short-stack justify-center items-center overflow-auto pt-8 p-16">
-      <div className="m-auto p-10 bg-accent-2 rounded-lg shadow-lg rounded-md">
-        <h1 className="text-3xl font-bold text-center text-accent-6 mb-8">
-          Groups
-        </h1>
-        <div className="flex flex-wrap justify-center gap-6 mt-4">
-          {groups.map((group, groupIndex) => (
-            <div key={groupIndex} className="bg-accent-3 p-6 rounded-md w-auto">
-              <div className="flex justify-between space-x-4">
-                <h2 className="text-2xl font-bold text-accent-6 mb-4">
-                  {group.name}
-                </h2>
-                <h2 className="text-accent-6 text-xl mb-4 mt-auto">
-                  {group.members.length} / {group.max_count} members
-                </h2>
-              </div>
+    return (
+        <div className="bg-accent-1 inline-flex font-short-stack justify-center items-center overflow-auto pt-8 p-16">
+            <div className="m-auto p-10 bg-accent-2 rounded-lg shadow-lg rounded-md">
+                <h1 className="text-3xl font-bold text-center text-accent-6 mb-8">
+                    Groups
+                </h1>
+                <div className="flex flex-wrap justify-center gap-6 mt-4">
+                    {groups.map((group, groupIndex) => (
+                        <div
+                            key={groupIndex}
+                            className="bg-accent-3 p-6 rounded-md w-auto"
+                        >
+                            <div className="flex justify-between space-x-4">
+                                <h2 className="text-2xl font-bold text-accent-6 mb-4">
+                                    {group.name}
+                                </h2>
+                                <h2 className="text-accent-6 text-xl mb-4 mt-auto">
+                                    {group.members.length} / {group.max_count}{" "}
+                                    members
+                                </h2>
+                            </div>
 
-              <p className="text-accent-6 text-lg mb-4">{group.description}</p>
-              <p className="text-accent-6 text-xl mb-4">
-                Location: {group.location}
-              </p>
-              <p className="text-accent-6 text-xl mb-4">Members:</p>
-              <ul className="text-accent-6 text-xl mb-4">
-                {group.members.map((member, index) => (
-                  <li key={index}>{member}</li>
-                ))}
-              </ul>
-              <p className="text-accent-6 text-xl mb-4">
-                {group.time} on {group.date}
-              </p>
-              {user && (
-                <button
-                  className="text-accent-2 bg-accent-5 p-2 rounded-md"
-                  onClick={() =>
-                    group.members.includes(auth?.currentUser?.displayName)
-                      ? leaveGroup(group.id)
-                      : joinGroup(group.id)
-                  }
-                >
-                  {group.members.includes(auth?.currentUser?.displayName)
-                    ? "Leave Group"
-                    : "Join Group"}
-                </button>
-              )}
+                            <p className="text-accent-6 text-lg mb-4">
+                                {group.description}
+                            </p>
+                            <p className="text-accent-6 text-xl mb-4">
+                                Location: {group.location}
+                            </p>
+                            <p className="text-accent-6 text-xl mb-4">
+                                Members:
+                            </p>
+                            <ul className="text-accent-6 text-xl mb-4">
+                                {group.members.map((member, index) => (
+                                    <li key={index}>{member}</li>
+                                ))}
+                            </ul>
+                            <p className="text-accent-6 text-xl mb-4">
+                                {group.time} on {group.date}
+                            </p>
+                            {user && (
+                                <button
+                                    className="text-accent-2 bg-accent-5 p-2 rounded-md"
+                                    onClick={() =>
+                                        group.members.includes(
+                                            auth?.currentUser?.displayName
+                                        )
+                                            ? leaveGroup(group.id)
+                                            : joinGroup(group.id)
+                                    }
+                                >
+                                    {group.members.includes(
+                                        auth?.currentUser?.displayName
+                                    )
+                                        ? "Leave Group"
+                                        : "Join Group"}
+                                </button>
+                            )}
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  );
+    );
 }
